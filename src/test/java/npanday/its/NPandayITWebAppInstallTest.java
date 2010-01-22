a743c3ad2e02c2536a4eefaaffc63f51bc49b155
Brett Porter <brett@apache.org>
2010-01-22 12:44:27 +0000

    no longer needs framework 3.5
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994300 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/npanday/its/NPandayITWebAppInstallTest.java
+++ src/test/java/npanday/its/NPandayITWebAppInstallTest.java
@@ -26,7 +26,7 @@ public class NPandayITWebAppInstallTest
 {
     public NPandayITWebAppInstallTest()
     {
-        super( "(1.0,)", "[v3.5,)" ); // 1.0.1+
+        super( "(1.0,)" ); // 1.0.1+
     }
 
     public void testWebAppInstall()
