13ee5dc6e0882c79e5821a718cf04f31434fcbc9
Adelita L. Padilla <apadilla@apache.org>
2010-01-28 09:37:48 +0000

    migrated npanday/trunk/integration-tests/it0032 to npanday-its/trunk/.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994341 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/npanday/its/it0032InstallationTest.java
@@ -0,0 +1,43 @@
+package npanday.its;
+
+/*
+ * Copyright 2009
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+public class it0032InstallationTest
+    extends AbstractNPandayIntegrationTestCase
+{
+    public it0032InstallationTest()
+    {
+        super( "(1.0,)" ); // 1.0.1+
+    }
+
+    public void testInstallationVerification()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0032" );
+        Verifier verifier = getVerifier( testDir );
+        verifier.executeGoal( "install" );
+        verifier.assertFilePresent( new File( testDir + "/" +
+            getAssemblyFile( "NPanday.IT.0032", "1.1.0.0", "dll" ) ).getAbsolutePath() );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+}
