689e861c0a3d4b2829dbebdedf07e57c423f19aa
Adelita L. Padilla <apadilla@apache.org>
2010-01-29 06:26:57 +0000

    removed failing ITs that causes build to break.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994351 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/resources/it0020/src/main/csharp/It0020.cs
+++
@@ -1,8 +0,0 @@
-namespace NPanday.IT {
-
-public class It0020 {
-	public static void Main () { 
-		System.Console.Write("Hello World!"); 
-	} 
-}
-}
--- src/test/resources/it0021/src/main/vb/it0021.vb
+++
@@ -1,8 +0,0 @@
-Imports org.apache.maven.it
-NameSpace NPanday.IT
-Public Class It0021
-	Public Sub New()
-       
-	End Sub
-End Class
-End NameSpace
