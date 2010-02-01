f15a8d41931c81c027070d9a155e4229c91ca05d
Adelita L. Padilla <apadilla@apache.org>
2010-02-01 13:52:01 +0000

    migrated IT0003.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994360 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/NPandayIT0003/src/main/csharp/It0003.cs
@@ -0,0 +1,16 @@
+namespace NPanday.IT {
+
+using System;
+
+public class It0003 {
+
+	public static void Main () { 
+		System.Console.Write("Hello World!");
+	}
+
+	public String GetValue()
+	{
+	    return "test";
+    }
+}
+}
