aea52a8f8c2942efe7e98b0b9289356f70955561
Josimpson Ancla Ocaba <jocaba@apache.org>
2010-05-07 10:19:53 +0000

    [12940] NPanday Fails to Resolve Dependencies if mixed versions of -SNAPSHOT and Released
    Added ITs
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/NPandayIT12940/src/main/csharp/Sample/MyApp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using TestLib;
+
+namespace Sample
+{
+    public class MyApp
+    {
+        public MyApp()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
