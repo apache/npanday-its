d69ea7cb4cda3f42ccb5077a7a44dfcb60a0bfb1
Josimpson Ancla Ocaba <jocaba@apache.org>
2010-05-13 07:44:06 +0000

    Added IT for NPandayIT13018
    Transitive Dependencies
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@994681 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/NPandayIT13018/src/main/csharp/Sample/NPandayIT13018.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using Sample;
+
+namespace Sample
+{
+    public class NPandayIT13018
+    {
+        public NPandayIT13018()
+        {
+            Console.WriteLine("Hello");
+            string dummy = Sample.MyApp.GetHello();
+        }
+    }
+}
