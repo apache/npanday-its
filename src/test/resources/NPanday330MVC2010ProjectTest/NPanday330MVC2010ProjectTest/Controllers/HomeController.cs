3207db8852e20e0ea6a5ac726fdc06cb7e123e97
Adelita L. Padilla <apadilla@apache.org>
2010-10-20 05:14:10 +0000

    [NPANDAY-328] - added integration test for VS2010 MVC Project
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@1024525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/NPanday330MVC2010ProjectTest/NPanday330MVC2010ProjectTest/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NPanday330MVC2010ProjectTest.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/test/resources/NPanday330MVC2010ProjectTest/NPanday330MVC2010ProjectTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NPanday330MVC2010ProjectTest.MvcApplication" Language="C#" %>
