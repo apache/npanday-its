3538cbe45f465e12eafb7162455eeab8681f2006
Lars Corneliussen <lcorneliussen@apache.org>
2011-05-03 08:14:02 +0000

    Set to use 1.4.0-incubating-SNAPSHOT
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@1098957 13f79535-47bb-0310-9956-ffa450edef68

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 if X%1==X set test=
 if not X%1==X set test=-Dtest=*%1*
 @echo on
-mvn test -Prun-its -Dnpanday.version=1.3.1-incubating-SNAPSHOT %test%
+mvn test -Prun-its -Dnpanday.version=1.4.0-incubating-SNAPSHOT %test%
