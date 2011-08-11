5108da5adf68e269791882fe9bf651c1b22fc72a
Brett Porter <brett@apache.org>
2011-08-11 14:07:24 +0000

    [NPANDAY-459] add an integration test for project references in MSBuild utilising projects
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/npanday-its/trunk@1156620 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/NPANDAY_459_MSBuildProjectReferences/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        ClassLibrary1.Class1 obj;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
